4d4bc13fefdd1d187ef3914c0b50450901e3da55
gastongouron <gastongouron@gmail.com>
2016-01-23 17:16:16 -0800

    Add json query

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -1,10 +1,21 @@
+require 'net/http'
+
 class MapsController < ApplicationController
 	def index
 		@my_location = MyLocation.new({lat: params["lat"], long: params["long"]})
-		
+
 		@points_of_interest = []
 		@destination = Destination.new({latitude: params[:latitude], longitude: params[:longitude]})
+
+    url = URI.parse('http://data.sfgov.org/resource/bm46-8iwk.json')
+    req = Net::HTTP::Get.new(url.to_s)
+    @res = Net::HTTP.start(url.host, url.port) {|http|
+    http.request(req)
+    }
+
+puts @res.body.to_s
+
 	end
-	
+
 end
 
