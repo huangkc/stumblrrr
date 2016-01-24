139eaf570a4195fbdea3d4969575864566174a79
gastongouron <gastongouron@gmail.com>
2016-01-23 18:49:33 -0800

    hello changes

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -1,13 +1,37 @@
+
+require 'net/http'
+
 class MapsController < ApplicationController
-	def index
+  def index
     p params
-		@my_location = MyLocation.new({lat: params["lat"], long: params["long"]})
+    @my_location = MyLocation.new({lat: params["lat"], long: params["long"]})
 
-		@points_of_interest = []
-		@destination = Destination.new({latitude: params[:latitude], longitude: params[:longitude]})
-    content = 'http://data.sfgov.org/resource/bm46-8iwk.json'
+    @points_of_interest = []
+    @destination = Destination.new({latitude: params[:latitude], longitude: params[:longitude]})
 
-	end
+    url = URI.parse('http://data.sfgov.org/resource/bm46-8iwk.json')
+    req = Net::HTTP::Get.new(url.to_s)
+    @res = Net::HTTP.start(url.host, url.port) {|http|
+    http.request(req)
+    }
+
+    @lol = JSON.parse(@res.body)
+    @lal = JSON.parse(@lol[1]['geometry'])
+    p '-------------------------------------'
+    p @lal['coordinates']
+
+    @address = params[:address] ||= 'Paris'
+
+  end
+
+
+  def create
+
+    @newaddress = params[:address]
+    redirect_to maps_path, :locals => {:address => @newaddress}
+    # render :template => 'maps/index', :locals => {:address => @newaddress}
+
+  end
+
 
 end
-
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
-  resources :maps 
+  resources :maps
 
 
 
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
 
   # Example of regular route:
     get 'maps' => 'maps#index'
+    post 'maps' => 'maps#create'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
