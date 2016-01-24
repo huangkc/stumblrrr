bbc10495a5a484ef9adefcd509b95e49cd718332
gastongouron <gastongouron@gmail.com>
2016-01-23 20:45:52 -0800

    All front shit

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -5,7 +5,7 @@ class MapsController < ApplicationController
   def index
     @my_location = MyLocation.new({lat: params["lat"], long: params["long"]})
 
-    @points_of_interest = PointsOfInterest.all
+    @points_of_interest = PointsOfInterest.all[1..30]
 
     @destination = Destination.new({latitude: params[:latitude], longitude: params[:longitude]})
 
@@ -13,25 +13,17 @@ class MapsController < ApplicationController
 
 
 
-p url = URI.parse('http://maps.google.com/maps/api/geocode/json?address=' + @address + '&sensor=false')
+url = URI.parse('http://maps.google.com/maps/api/geocode/json?address=' + @address + '&sensor=false')
 req = Net::HTTP::Get.new(url.to_s)
 res = Net::HTTP.start(url.host, url.port) {|http|
   http.request(req)
 }
 
+ puts res.body
+
 
 
 
   end
-
-
-  # def create
-
-  #   @newaddress = params[:address]
-  #   redirect_to maps_path, :locals => {:address => @newaddress}
-  #   # render :template => 'maps/index', :locals => {:address => @newaddress}
-
-  # end
-
 
 end
