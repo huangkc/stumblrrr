815512d767d8880a62588c305120302c14274e16
gastongouron <gastongouron@gmail.com>
2016-01-23 17:43:52 -0800

    changes

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -1,25 +1,11 @@
-require 'net/http'
-
 class MapsController < ApplicationController
 	def index
 		@my_location = MyLocation.new({lat: params["lat"], long: params["long"]})
 
 		@points_of_interest = []
 		@destination = Destination.new({latitude: params[:latitude], longitude: params[:longitude]})
+    content = 'http://data.sfgov.org/resource/bm46-8iwk.json'
 
-    url = URI.parse('http://data.sfgov.org/resource/bm46-8iwk.json')
-    req = Net::HTTP::Get.new(url.to_s)
-    @res = Net::HTTP.start(url.host, url.port) {|http|
-    http.request(req)
-    }
-
-    @lol = JSON.parse(@res.body)
-
-    @lal = JSON.parse(@lol[1]['geometry'])
-
-    p '-------------------------------------'
-
-    p @lal['coordinates']
 	end
 
 end
