44d00aad0f526158fadc32e5424a4741380e3027
Eric DellAringa <edella2@gmail.com>
2016-01-23 18:42:22 -0800

    add database, drop and recreate

--- db/migrate/20160123211625_create_destinations.rb
+++ db/migrate/20160123211625_create_destinations.rb
@@ -1,10 +1,13 @@
 class CreateDestinations < ActiveRecord::Migration
   def change
     create_table :destinations do |t|
-    	t.string :latitude
-    	t.string :longitude
-    	t.string :title
-
+    	t.string :lat
+      t.string :long
+      t.string :title
+      t.string :location_description
+      t.string :medium
+      t.string :source
+      t.string :artist
       t.timestamps null: false
     end
   end
